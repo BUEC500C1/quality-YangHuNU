ce35c1da3f7942c1dda1680db39ad4eb591e2519
YangHuNU <42973499+YangHuNU@users.noreply.github.com>
2020-01-26 13:03:40 -0500

    Update test_cases.py

--- test_cases.py
+++ test_cases.py
@@ -2,4 +2,8 @@ import pytest
 from arabic_to_roman import int_to_Roman
 
 def test_c():
-  assert int_to_Roman(10) == 'X'
+  val = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+  syb = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+  
+  for i in range(0, 13):
+    assert int_to_Roman(val[i]) == syb[i]
