ca01a6a3a1d0cc8b56897d055430364a019b026e
YangHuNU <42973499+YangHuNU@users.noreply.github.com>
2020-01-26 12:13:28 -0500

    Update arabic_to_roman.py

--- arabic_to_roman.py
+++ arabic_to_roman.py
@@ -1,22 +1,15 @@
-class arabic_to_roman:
-    def atr(self, num):
-        val = [
-            1000, 900, 500, 400,
-            100, 90, 50, 40,
-            10, 9, 5, 4,
-            1
-            ]
-        syb = [
-            "M", "CM", "D", "CD",
-            "C", "XC", "L", "XL",
-            "X", "IX", "V", "IV",
-            "I"
-            ]
-        roman_num = ''
-        i = 0
-        while  num > 0:
-            for _ in range(num // val[i]):
-                roman_num += syb[i]
-                num -= val[i]
-            i += 1
-        return roman_num
+def int_to_Roman(self, n):
+    val = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    syb = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+    roman_num = ''
+    i = 0
+    while  num > 0:
+        for _ in range(n // val[i]):
+            roman_num += syb[i]
+            n -= val[i]
+        i += 1
+    return roman_num
+
+if __name__ == '__main__':
+    for case in range(0,10000):
+        print(int_to_Roman(case))
